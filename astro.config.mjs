37e8c9524ae9dc80f9a575ec6271f94d7303f57f
devgoagency-arch <devgoagency@gmail.com>
2026-05-29 15:10:26 -0500

    feat: apply SEO audit fixes including sitemap canonicals metadata and alt tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://wholesale.fiorefoodscanada.com',
+  integrations: [tailwind(), sitemap()],
   image: {
     domains: ['fiorefoodscanada.com']
   }
